1a4c2387349d9e2dac51e4d7c3f9b0711106f928
tuananh-ng <158744840+tuananh-ng@users.noreply.github.com>
2025-01-31 15:33:32 +0100

    Implement a function to remove items from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removedItems) {
+    if (!Array.isArray(array)) { return ;}
+
+    for (const removedItem of removedItems) {
+        array = array.filter((item) => item !== removedItem);
+    }
+    return array;
 };
 
 // Do not edit below this line
