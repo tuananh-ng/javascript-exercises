568776f20695ab02ca3bcce34a5de91d7b090093
tuananh-ng <158744840+tuananh-ng@users.noreply.github.com>
2025-01-31 15:53:25 +0100

    Implement a function to sum all intergers in a range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end)) { return 'ERROR'; }
+    if (start < 0 || end < 0) { return 'ERROR'; }
 
+    let sum = 0;
+    let trueStart = (start < end ? start : end), trueEnd = (end > start ? end : start);
+    for (let i = trueStart; i <= trueEnd; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
