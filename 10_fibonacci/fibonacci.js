56b1ec0f163707554771b493279757ff2052875b
tuananh-ng <158744840+tuananh-ng@users.noreply.github.com>
2025-02-08 15:34:29 +0100

    a function that returns a specific member of the Fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0) return 'OOPS';
+    
+    index = +index;
+    const fibSequence = [0, 1];
+    if (index < 2) return fibSequence[index];
 
+    updateFibSequence(index);
+    return fibSequence[index];
+
+    function updateFibSequence(num) {
+        let nextFibNum;
+        for (let i = 1; i < num; i++) {
+            nextFibNum = fibSequence.at(-1) + fibSequence.at(-2);
+            fibSequence.push(nextFibNum);
+        }
+    }
 };
 
 // Do not edit below this line
